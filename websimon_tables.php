c7f8b9f1bfe5acc45e1e41a570717ff41f452efd
websimonr <websimonr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-19 18:56:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/websimon-tables/trunk@492281 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- websimon_tables.php
+++ websimon_tables.php
@@ -3,7 +3,7 @@
 Plugin Name: Websimon Tables
 Plugin URI: http://websimon.se/websimon-tables
 Description: Create and style tables for wordpress posts and pages
-Version: 1.0
+Version: 1.01
 Author: Simon Rybrand
 Author URI: http://websimon.se
 */
